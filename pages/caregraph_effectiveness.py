93d6b50ff00708ce0bd417312b88f530367a3ed4
Jin-Seoung <jinseoungoh@gmail.com>
2025-07-03 15:40:24 +0900

    Update caregraph_effectiveness.py

--- pages/caregraph_effectiveness.py
+++ pages/caregraph_effectiveness.py
@@ -4,7 +4,7 @@ import datetime
 import joblib
 import os
 
-from pages.tool import CareGraph, MemoryAgent, _4oMiniClient 
+from pages.tool import CareGraph, MemoryAgent, _4oMiniClient, UserProfile
 from my_switch import switch_page
 
 # --- Helper functions ---
