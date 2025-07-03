72e73e589c4c4fdb77f3165eb13754fb6d9b527b
Jin-Seoung <jinseoungoh@gmail.com>
2025-07-03 15:01:31 +0900

    Update survey_4.py

--- pages/survey_4.py
+++ pages/survey_4.py
@@ -68,7 +68,7 @@ if st.button("제출"):
 col1, col2 = st.columns([1, 1])
 with col1:
     if st.button("◀ 이전 페이지"):
-        st.switch_page("pages/survey_3")       # pages/home.py (확장자 제외)
+        st.switch_page("pages/survey_3.py")       # pages/home.py (확장자 제외)
 with col2:
     if st.button("다음 페이지 ▶"):
-        st.switch_page("pages/survey_5")    # pages/survey2.py (확장자 제외)
+        st.switch_page("pages/survey_5.py")    # pages/survey2.py (확장자 제외)
