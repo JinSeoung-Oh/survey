e67e7574c958228f34d6217785f5438702d79a61
Jin-Seoung <jinseoungoh@gmail.com>
2025-07-23 21:53:43 +0900

    Update 4_improve_survey.py

--- pages/4_improve_survey.py
+++ pages/4_improve_survey.py
@@ -198,7 +198,7 @@ elif st.session_state.state == "survey":
                 f.write("timestamp","expert_id","intervention_effectiveness","feedback_reflection","fatigue","trust_in_strategy","clarity","additional_comments\n")
 
         with open(FILEPATH, "a", encoding="utf-8") as f:
-            f.write(f"{now},{expert_id}",f"{q1},{q2},{q3},{q4},{q5}",f"\"{comment}\"\n")
+            f.write(f"{now},{expert_id},"f"{q1},{q2},{q3},{q4},{q5},"f"\"{comment}\"\n")
 
         st.session_state.survey4_submitted = True
         st.success("설문이 저장되었습니다. 감사합니다!")
